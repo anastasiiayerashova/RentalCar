97d950e70395b8ebc4c38323b669d14e0e6f1e8a
Anastasiia Yerashova <yerashova.a@gmail.com>
2025-04-14 02:09:26 +0200

    fix icon

--- src/components/FavouriteButton/FavouriteButton.jsx
+++ src/components/FavouriteButton/FavouriteButton.jsx
@@ -6,9 +6,15 @@ const FavouriteButton = ({isFavourite, onClick}) => {
 
     return (
         <button className={s.icon_btn} onClick={onClick}>
-            <svg width={16} height={16}>
-                <use href={isFavourite ? `${svgIcon}#icon-heart-active` : `${svgIcon}#icon-heart`} />
-            </svg>
+           {isFavourite ? (
+              <svg width={16} height={16} viewBox="0 0 32 32">
+                 <path fill="#3470ff" d="M16 29s-13-8.35-13-17.38C3 6.42 8.42 3 13.12 5.46 15.21 6.57 16 8.28 16 8.28s0.79-1.71 2.88-2.82C23.58 3 29 6.42 29 11.62 29 20.65 16 29 16 29z"/>
+              </svg>
+            ) : (
+              <svg width={16} height={16}>
+                 <use href={`${svgIcon}#icon-heart`} />
+              </svg>
+            )}
         </button>
     )
 }
